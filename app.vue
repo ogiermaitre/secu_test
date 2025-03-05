e80e54b75bbe6def7d896fb20b6c8bf8e4e1a276
Ogier Maitre <ogier.maitre@gmail.com>
2025-03-05 16:53:20 +0100

    add SharedArrayBuffer example in app.vue component

--- app.vue
+++ app.vue
@@ -4,3 +4,14 @@
     <NuxtWelcome />
   </div>
 </template>
+<script lang="js">
+
+export default defineComponent({
+  setup() {
+    const a = new SharedArrayBuffer(1024)
+    console.log(a)
+    return {}
+  }
+})
+
+</script>
